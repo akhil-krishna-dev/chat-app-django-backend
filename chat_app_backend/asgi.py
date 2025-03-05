3a11179d695c08da14fd5739d2fb52dd3d7e1ea4
akhil-krishna-dev <akhilkrishna36912@gmail.com>
2025-03-05 20:05:09 +0530

    changes in settings.py file cors middleware moved to top of the list, added CORS_ALLOWED_ORIGINS variable to .env file

--- chat_app_backend/asgi.py
+++ chat_app_backend/asgi.py
@@ -1,11 +1,15 @@
 import os
+from django import setup
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_app_backend.settings')
+setup()
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from Home.routing import websocket_urlpatterns
 from Accounts.channels_middleware import JWTWebsocketMiddleware
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_app_backend.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
--- chat_app_backend/settings.py
+++ chat_app_backend/settings.py
@@ -48,9 +48,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -59,9 +59,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-]
+CORS_ALLOWED_ORIGINS = config('CORS_ALLOWED_ORIGINS', default=[]).split(",")
 
 ROOT_URLCONF = 'chat_app_backend.urls'
 
